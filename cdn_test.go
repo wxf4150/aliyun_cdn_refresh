e0b0039f45a89f2622bc8e87bd503bc00cc201ed
伍晓飞 <wxf4150@163.com>
2017-09-11 09:33:36 +0800

    set accessKeyId accessKeySecret as parameter

--- cdn_test.go
+++ cdn_test.go
@@ -3,7 +3,8 @@ package ali_cdn_refresh
 import "testing"
 
 func Test_RefrechUrl(t *testing.T) {
-	err:=RefrechUrl("cdn.vrcdkj.cn/Act-Snapshot/7867e4ffbb454a5f98f5fabee99de4e7/1504749066455tw8s84n09tz9jejjtw0b8d7vi.mp4")
+	accessKeyId, accessKeySecret:="",""
+	err:=RefrechUrl(accessKeyId, accessKeySecret,"cdn.vrcdkj.cn/Act-Snapshot/7867e4ffbb454a5f98f5fabee99de4e7/1504749066455tw8s84n09tz9jejjtw0b8d7vi.mp4")
 t.Log(err)
 }
 
